3798d3b1cc339e1b46ea0dc938ac5e75b0c94335
mhmd-moradi <mohamad.muradi98@gmail.com>
2025-02-27 16:56:58 +0100

    chore: add global variables

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ import cv2
 import re
 import datetime
 from imaging_interview import preprocess_image_change_detection, compare_frames_change_detection
+
+invalid_images = []
+image_folder = os.path.expanduser("~/Downloads/dataset-candidates-ml/dataset") # dataset folder example path
+min_contour_area = 200 # Minimum contour area to detect as a change
+change_threshold = 800 # Change score threshold to trigger difference detection
