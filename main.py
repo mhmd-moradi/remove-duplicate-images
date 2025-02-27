2c8c161e3f52a77a14fe59e2a271f1deeb7f034f
mhmd-moradi <mohamad.muradi98@gmail.com>
2025-02-27 17:00:56 +0100

    feat(preprocessing): add extracting image resolutions

--- main.py
+++ main.py
@@ -15,3 +15,19 @@ def convert_to_yyyymmddhhmmss(timestamp_ms):
     """Convert Unix timestamp in milliseconds to YYYYMMDDHHMMSS format."""
     dt = datetime.datetime.fromtimestamp(int(timestamp_ms) / 1000)
     return dt.strftime("%Y%m%d%H%M%S")
+
+def load_image(image_folder, filename):
+    """Load an image from the given folder and filename."""
+    return cv2.imread(os.path.join(image_folder, filename))
+
+def images_resolutions(images_by_camera_id):
+    """Get the resolutions of the images for each camera ID."""
+    for camera_id in images_by_camera_id:
+        print(f"Image Resolutions by Camera ID: {camera_id}")
+        resolutions = {}
+        for img in tqdm(images_by_camera_id[camera_id]):
+            image = load_image(image_folder, images_by_camera_id[camera_id][img])
+            if (image.shape[1], image.shape[0]) not in resolutions:
+                resolutions[(image.shape[1], image.shape[0])] = 0
+            resolutions[(image.shape[1], image.shape[0])] += 1
+        print(f"Camera ID: {camera_id} - Image Resolutions: {resolutions} \n")
